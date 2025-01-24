626dccb3ff1d1d620276c1541ffc0d0c76df6d5a
Ariana Martin <arianamartinmartinez@gmail.com>
2025-01-24 23:57:22 +0100

    feat: add structure to index

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 require("dotenv").config();
+const singersRouter = require("./src/api/routes/singers");
+const songsRouter = require("./src/api/routes/songs");
+const { connectDB } = require("./src/config/db");
 
 const express = require("express");
 const app = express();
 
+//express.json so de server is able to get data in json format
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/v1/songs", songsRouter);
+app.use("/api/v1/singers", singersRouter);
+
+app.use("*", (req, res, next) => {
+    return res.status(404).json("Route not found");
+});
+
 app.listen(3000, () => {
-    console.log("Servidor desplegado en http://localhost:3000");
-})
+    console.log("Server deployed in http://localhost:3000");
+});
